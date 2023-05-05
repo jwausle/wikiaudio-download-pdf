d0e335552a0da1757d476a892d76547ab546ffe2
Jan Winter <winter@jwausle.de>
2023-05-05 12:58:10 +0200

    Inital copy of git@github.com:Azure-Samples/fastapi-on-azure-functions.git

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,21 @@
-def main(req):
-    user = req.params.get('user')
-    return f'Hello, {user}!'
+import logging
+import azure.functions as func
+from FastAPIApp import app  # Main API application
+
+
+@app.get("/sample")
+async def index():
+    return {
+        "info": "Try /hello/Shivani for parameterized route.",
+    }
+
+
+@app.get("/hello/{name}")
+async def get_name(name: str):
+    return {
+        "name": name,
+    }
+
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(app).handle_async(req, context)
